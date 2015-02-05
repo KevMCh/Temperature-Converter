7442ddec67353ed8f505b1bfe2b6c3adcdf44da2
Kevin Martín Chinea <alu0100774665@ull.edu.es>
2015-02-05 22:41:45 +0000

    Script

---
+++ temperature.js
@@ -0,0 +1,39 @@
+"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+function calculate() {
+  var result;
+  var original       = document.getElementById("original");
+  var temp = original.value;
+  var regexp = /^\s*([-+]?\d+(?: \.\d+)?(?:e[-+]?\d+)?)\s*([cf])\s*$/i;
+  
+  /*
+  Función match, se le pasa una expresión regular a una cadena
+   y nos devuelve las palabras que coinciden con  la expresión.
+  */
+
+  var m = temp.match(regexp);
+  
+  if (m) {
+
+  	/*
+  	Seleccionamos por un lado el número, y por otro el tipo de valor 
+  	(Farenheit o  Celsius)
+
+	*/
+
+    var num = m[1];
+    var type = m[2];
+    num = parseFloat(num);
+    if (type == 'c' || type == 'C') {
+      result = (num * 9/5)+32;
+      result = result.toFixed(1)+" Farenheit"
+    }
+    else {
+      result = (num - 32)*5/9;
+      result = result.toFixed(1)+" Celsius"
+    }
+    converted.innerHTML = result;
+  }
+  else {
+    converted.innerHTML = "Error!";
+  }
+}
